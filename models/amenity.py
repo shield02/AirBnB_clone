73f85b2f75857b61db519381eb6015fc8eeeb207
shield <myshield02@gmail.com>
2023-07-23 19:58:10 +0100

    code for the amenity model

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Amenity class model"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Amenity class definition
+    Attrs:
+        name (str): amenity name
+    """
+    name = ""
