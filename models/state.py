62f2fb27494bc8148c8067052c57336a51b4003f
shield <myshield02@gmail.com>
2023-07-23 19:59:23 +0100

    class definition for state

---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""State class model"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """State class definition
+    Attrs:
+        name (str): state name
+    """
+    name = ""
